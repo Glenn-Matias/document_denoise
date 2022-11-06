acc2fdabc166de1aad8367c2761e5e2972fba31a
Glenn Matias <glennmatias@Glenns-MacBook-Pro.local>
2022-11-06 19:04:19 +0800

    added viz

--- scripts/script.py
+++ scripts/script.py
@@ -3,14 +3,15 @@ import numpy as np
 
 def read_image(path):
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-
-    return img
+    width = img.shape[1]
+    height = img.shape[0]
+    return img, width, height
 
 def resize_image(img, scale=0.5):    
     width = int(img.shape[1] * scale)
     height = int(img.shape[0] * scale)
     resized = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
-    return resized
+    return resized, width, height
 
 
 def dilate_image(img, kernel_size=10):
@@ -35,6 +36,11 @@ def invert_image(img):
 
     return inverted_img
 
+def add_text_to_image(img, text):
+    img = cv2.putText(img, text, (20,100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3, cv2.LINE_AA)
+    return img
+
+
 # Declare constants
 DILATE_KERNEL_SIZE = 21
 IMAGE_SCALE = 0.5
@@ -51,16 +57,18 @@ NOISE_TYPE = SHADOW
 
 # for image_number in range(1,11):
 for image_number in range(1,11):
-    # if image_number!=10: continue
+    if image_number!=10: continue
     filename = f"{ROOT_DIR}/{NOISE_TYPE}/{image_number}.{EXTENSION}"
 
-    img = read_image(filename)
+    img, width, height = read_image(filename)
     original_image = img
 
-    img = dilate_image(img, kernel_size=DILATE_KERNEL_SIZE)
-    # img = blur_image(img)
-    img = subtract_images(img, original_image)
-    img = invert_image(img)
+    dilated_img = dilate_image(img, kernel_size=DILATE_KERNEL_SIZE)
+    blurred_img = blur_image(dilated_img)
+    subtracted_img = subtract_images(blurred_img, original_image)
+    inverted_img = invert_image(subtracted_img)
+
+    img = inverted_img
 
     ret, binary_img = cv2.threshold(img,170,255, cv2.THRESH_BINARY) 
     ret, otsu_img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU) # 2nd param doesnt matter
@@ -68,14 +76,36 @@ for image_number in range(1,11):
     adapt_img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,101,40)
 
     # Resize for viewport
-    original_image = resize_image(original_image, scale=IMAGE_SCALE)
-    img = resize_image(img, scale=IMAGE_SCALE)
-    binary_img = resize_image(binary_img, scale=IMAGE_SCALE)
-    adapt_img = resize_image(adapt_img, scale=IMAGE_SCALE)
-    otsu_img = resize_image(otsu_img, scale=IMAGE_SCALE)
+    original_image, scaled_width, scaled_height = resize_image(original_image, scale=IMAGE_SCALE)
+    original_image = add_text_to_image(original_image, "Original")
+
+    dilated_img, _, _ = resize_image(dilated_img, scale=IMAGE_SCALE)
+    dilated_img = add_text_to_image(dilated_img, "Dilated")
+
+    blurred_img, _, _ = resize_image(blurred_img, scale=IMAGE_SCALE)
+    blurred_img = add_text_to_image(blurred_img, "Blurred")
+
+    subtracted_img, _, _ = resize_image(subtracted_img, scale=IMAGE_SCALE)
+    subtracted_img = add_text_to_image(subtracted_img, "Subtracted")
+
+    inverted_img, _, _ = resize_image(inverted_img, scale=IMAGE_SCALE)
+    inverted_img = add_text_to_image(inverted_img, "Inverted")
+
+    before_and_after_img = np.hstack((original_image, dilated_img, blurred_img, subtracted_img, inverted_img))
+    cv2.imshow(f'Steps before Thresholding {filename}', before_and_after_img)
+
+    binary_img, _, _ = resize_image(binary_img, scale=IMAGE_SCALE)
+    binary_img = add_text_to_image(binary_img, "Binary Threshold")
+
+    adapt_img, _, _ = resize_image(adapt_img, scale=IMAGE_SCALE)
+    adapt_img = add_text_to_image(adapt_img, "Adaptive Threshold")
+
+    otsu_img, _, _ = resize_image(otsu_img, scale=IMAGE_SCALE)
+    otsu_img = add_text_to_image(otsu_img, "Otsu's Threshold")
 
     before_and_after_img = np.hstack((original_image, binary_img, adapt_img, otsu_img))
-    cv2.imshow(f'Image size {filename}', before_and_after_img)
+    cv2.imshow(f'After Thresholding {filename}', before_and_after_img)
+
 
 
 cv2.waitKey(0)
